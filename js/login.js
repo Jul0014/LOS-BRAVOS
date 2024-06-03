2ff373e973d226176e94106c4c621bcc0bdaeb4b
Jul0014 <jberriosb@ulasalle.edu.pe>
2024-06-03 00:34:37 -0500

    Index y Login terminados

---
+++ js/login.js
@@ -0,0 +1,24 @@
+const usuariosPermitidos = [
+    { correo: "usuario1@example.com", contraseña: "contraseña1" },
+    { correo: "usuario2@example.com", contraseña: "contraseña2" },
+    { correo: "usuario3@example.com", contraseña: "contraseña3" }
+];
+
+function validarCredenciales(correo, contraseña) {
+    return usuariosPermitidos.some(usuario => usuario.correo === correo && usuario.contraseña === contraseña);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const correo = document.getElementById("email").value;
+    const contraseña = document.getElementById("password").value;
+
+    if (validarCredenciales(correo, contraseña)) {
+        window.location.href = "./index.html";
+    } else {
+        document.getElementById("error-message").textContent = "Credenciales incorrectas. Por favor, intenta de nuevo.";
+    }
+}
+
+document.getElementById("login-form").addEventListener("submit", handleSubmit);
